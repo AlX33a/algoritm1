0355ca3e24a3bdabdc4b7f701c8fa5028f549e78
Alexsahaa <36375661+AlX33a@users.noreply.github.com>
2022-02-25 23:58:59 +0300

    Update Sort_by_exchange_method.py

--- Sort_by_exchange_method.py
+++ Sort_by_exchange_method.py
@@ -1,1 +1,12 @@
-
+def selection_sort(lst: list) -> list:
+ 
+    operations_count = 0
+ 
+    for i in range(len(lst)-1):
+        for j in range(i+1, len(lst)):
+            if lst[j] < lst[i] and i < j:
+                lst[i], lst[j] = lst[j], lst[i]
+            operations_count += 1
+ 
+    print('Кол-во операций (выбором): ', operations_count)
+    return lst
