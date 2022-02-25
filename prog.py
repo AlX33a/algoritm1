236f4749fa7297869aad4bc222393d9d347dc15f
Alexsahaa <36375661+AlX33a@users.noreply.github.com>
2022-02-25 23:46:06 +0300

    Create prog.py

---
+++ prog.py
@@ -0,0 +1,15 @@
+a = input("a = ")
+b = input("b = ")
+
+
+summ = a + b
+diff = a - b
+comp = a * b
+divi = a / b
+ 
+print(f"""
+Сумма: {summ}
+Разность: {diff}
+Произведение: {comp}
+Частное: {divi}
+""")
