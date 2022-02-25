f2290b4449219f4afb3e0cb119a483635d4f834f
Alexsahaa <36375661+AlX33a@users.noreply.github.com>
2022-02-26 00:00:01 +0300

    Update Shell_sort.py

--- Shell_sort.py
+++ Shell_sort.py
@@ -1,1 +1,15 @@
-
+def shell_sort(lst: list) -> list:
+    step = len(lst) // 2
+    operations_count = 0
+ 
+    while step > 0:
+        for i in range(step, len(lst)):
+            current_value = lst[i]
+ 
+            if lst[i - step] > current_value:
+                lst[i - step], lst[i] = lst[i], lst[i - step]
+            operations_count += 1
+        step //= 2
+ 
+    print('Кол-во операций (Шелл): ', operations_count)
+    return lst
