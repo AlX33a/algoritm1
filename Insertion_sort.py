35936a8faa979ce420f8acddd64b5b18f49af58d
Alexsahaa <36375661+AlX33a@users.noreply.github.com>
2022-02-26 00:02:12 +0300

    Update Insertion_sort.py

--- Insertion_sort.py
+++ Insertion_sort.py
@@ -1,1 +1,9 @@
-
+def insertion_sort(lst: list) -> list:
+    n = len(lst)
+    operations_count = 0
+    for i in range(1,n):
+        for j in range(i, 0, -1):
+            operations_count += 1
+            if lst[j] < lst[j-1]:
+                lst[j], lst[j-1] = lst[j-1], lst[j]
+            else: break
